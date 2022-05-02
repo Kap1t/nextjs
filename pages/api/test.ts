d8ad0db1a7e5fb28a43ae62e949ae29a2d6eefcb
Kap1t <tolyapashencev@gmail.com>
2022-05-02 06:06:35 +0300

    dsadsafsagkgklplz

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -9,12 +9,12 @@ export default function test(req: NextApiRequest, res: NextApiResponse) {
     res.setHeader(
       "Set-Cookie",
       cookie.serialize("myToken", "dsadadawd2d24", {
-        httpOnly: true,
-        secure: false,
-        sameSite: "strict",
+        // httpOnly: true,
+        // secure: false,
+        // sameSite: "strict",
         maxAge: 60 * 20,
         path: "/",
-        domain: ".vercel.app",
+        // domain: ".vercel.app",
       })
     );
     res.status(200).json({ message: "loveYou" });
