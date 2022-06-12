ab7b072645c63448ac53e11f33863027a9e2f087
Kap1t <tolyapashencev@gmail.com>
2022-06-12 06:51:59 +0300

    11

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import ContextWrapper from "../Context/ContextWrapper";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ContextWrapper>
+      <Header />
       <Component {...pageProps} />
+      <Footer />
     </ContextWrapper>
   );
 }
